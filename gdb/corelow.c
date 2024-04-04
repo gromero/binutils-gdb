65ec655b10a8b856d76e45f3690866955bed5759
Gustavo Romero <gustavo.romero@linaro.org>
2024-04-04 04:31:42 +0000

    gdb: Introduce is_address_tagged target hook
    This commit introduces a new target hook, target_is_address_tagged,
    which is used instead of the gdbarch_tagged_address_p gdbarch hook in
    the upper layer (printcmd.c).
    
    This change allows the memory tagging address checking to be specialized
    easily per target in the future. Since target_is_address_tagged
    continues to use the gdbarch_tagged_address_p hook there is no change
    in behavior for the targets using the new target hook (the remote.c,
    aarch64-linux-nat.c, and corelow.c targets).
    
    This change enables easy specialization of memory tagging address
    check per target in the future. As target_is_address_tagged continues
    to utilize the gdbarch_tagged_address_p hook, there is no change in
    behavior for all the targets that use the new target hook (i.e., the
    remote.c, aarch64-linux-nat.c, and corelow.c targets).
    
    Just the gdbarch_tagged_address_p signature is changed for convenience,
    since target_is_address_tagged takes the address to be checked as a
    CORE_ADDR type.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>

--- gdb/corelow.c
+++ gdb/corelow.c
@@ -109,6 +109,8 @@ public:
   bool fetch_memtags (CORE_ADDR address, size_t len,
 		      gdb::byte_vector &tags, int type) override;
 
+  bool is_address_tagged (gdbarch *gdbarch, CORE_ADDR address) override;
+
   x86_xsave_layout fetch_x86_xsave_layout () override;
 
   /* A few helpers.  */
@@ -1408,6 +1410,12 @@ core_target::fetch_memtags (CORE_ADDR address, size_t len,
   }
 
   return false;
+}
+
+bool
+core_target::is_address_tagged (gdbarch *gdbarch, CORE_ADDR address)
+{
+  return gdbarch_tagged_address_p (gdbarch, address);
 }
 
 /* Implementation of the "fetch_x86_xsave_layout" target_ops method.  */
